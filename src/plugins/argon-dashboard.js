ae8a7d4585cd006bb0e95d947b06b3179f219f7a
cristij <joracristi@gmail.com>
2019-04-09 20:45:55 +0300

    Add fontawesome as external link Cleanup fontawesome from vendors

--- src/plugins/argon-dashboard.js
+++ src/plugins/argon-dashboard.js
@@ -1,5 +1,4 @@
 import "@/assets/vendor/nucleo/css/nucleo.css";
-import "@/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "@/assets/scss/argon.scss";
 import globalComponents from "./globalComponents";
 import globalDirectives from "./globalDirectives";
