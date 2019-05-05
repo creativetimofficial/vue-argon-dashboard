639719b20c3504d872185ffca271bbe1cd6a8306
cristij <joracristi@gmail.com>
2019-05-05 15:18:01 +0300

    Updates

--- documentation/assets/js/5.69216089.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{567:function(n,w,o){}}]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vue-argon-dashboard/precache-manifest.3e4a16709e31bc770f9d4ce8797335ac.js"
+  "/vue-argon-dashboard/precache-manifest.df533db85572ffd21df46c83d595a116.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
