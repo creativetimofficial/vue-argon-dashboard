3863ae9a5fbc8f384b881d257d02afb713121af9
Toma Rares <tomairares@gmail.com>
2021-02-16 16:12:58 +0200

    Feat: Add baseheader

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,8 +1,10 @@
+import BaseHeader from "../components/BaseHeader";
 import Card from "../components/Card";
 import StatsCard from "../components/StatsCard";
 
 const GlobalComponents = {
   install(app) {
+    app.component("base-header", BaseHeader);
     app.component("card", Card);
     app.component("stats-card", StatsCard);
   },
