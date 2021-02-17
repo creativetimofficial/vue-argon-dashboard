29aef0b396656a4acaf5104030127a99da280b9f
Toma Rares <tomairares@gmail.com>
2021-02-17 11:15:10 +0200

    Feat: Add basetable

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <table class="table tablesorter" :class="tableClass">
+    <thead :class="theadClasses">
+      <tr>
+        <slot name="columns" :columns="columns">
+          <th v-for="column in columns" :key="column">{{ column }}</th>
+        </slot>
+      </tr>
+    </thead>
+    <tbody :class="tbodyClasses">
+      <tr v-for="(item, index) in data" :key="index">
+        <slot :item="item">
+          <td v-for="column in columns" :key="column">
+            <template v-if="hasValue(item, column)">
+              {{ itemValue(item, column) }}
+            </template>
+          </td>
+        </slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'base-table',
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+      description: 'Table columns'
+    },
+    data: {
+      type: Array,
+      default: () => [],
+      description: 'Table data'
+    },
+    type: {
+      type: String, // striped | hover
+      default: '',
+      description: 'Whether table is striped or hover type'
+    },
+    theadClasses: {
+      type: String,
+      default: '',
+      description: '<thead> css classes'
+    },
+    tbodyClasses: {
+      type: String,
+      default: '',
+      description: '<tbody> css classes'
+    }
+  },
+  computed: {
+    tableClass() {
+      return this.type && `table-${this.type}`;
+    },
+    colsWithValue() {
+      return (row) => {
+        return this.columns.filter(column => this.hasValue(row, column))
+      }
+    }
+  },
+  methods: {
+    hasValue(item, column) {
+      return item[column.toLowerCase()] !== 'undefined';
+    },
+    itemValue(item, column) {
+      return item[column.toLowerCase()];
+    }
+  }
+};
+</script>
+<style></style>
--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,5 +1,6 @@
 import BaseHeader from "../components/BaseHeader";
 import BaseNav from "../components/BaseNav";
+import BaseTable from "../components/BaseTable";
 import Card from "../components/Card";
 import StatsCard from "../components/StatsCard";
 
@@ -7,6 +8,7 @@ const GlobalComponents = {
   install(app) {
     app.component("base-header", BaseHeader);
     app.component("base-nav", BaseNav);
+    app.component("base-table", BaseTable);
     app.component("card", Card);
     app.component("stats-card", StatsCard);
   }
