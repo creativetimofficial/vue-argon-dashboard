0ff3048162a44581f7a17c4c31c3e0836144f5b2
Toma Rares <tomairares@gmail.com>
2021-02-17 15:17:59 +0200

    Feat: add base input

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,5 +1,6 @@
 import BaseButton from "../components/BaseButton";
 import BaseHeader from "../components/BaseHeader";
+import BaseInput from "../components/BaseInput";
 import BaseNav from "../components/BaseNav";
 import BaseProgress from "../components/BaseProgress";
 import BaseTable from "../components/BaseTable";
@@ -10,6 +11,7 @@ const GlobalComponents = {
   install(app) {
     app.component("base-button", BaseButton);
     app.component("base-header", BaseHeader);
+    app.component("base-input", BaseInput);
     app.component("base-nav", BaseNav);
     app.component("base-progress", BaseProgress);
     app.component("base-table", BaseTable);
