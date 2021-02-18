f2663be9a755e382323e09fd2742160b9036fc1f
Toma Rares <tomairares@gmail.com>
2021-02-18 15:06:17 +0200

    Refactor: Remove fade transition

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -62,10 +62,8 @@
       <dashboard-navbar></dashboard-navbar>
 
       <div @click="toggleSidebar">
-        <fade-transition :duration="200" origin="center top" mode="out-in">
           <!-- your content here -->
           <router-view></router-view>
-        </fade-transition>
         <content-footer v-if="!$route.meta.hideFooter"></content-footer>
       </div>
     </div>
@@ -74,13 +72,11 @@
 <script>
 import DashboardNavbar from "./DashboardNavbar.vue";
 import ContentFooter from "./ContentFooter.vue";
-// import { FadeTransition } from 'vue2-transitions';
 
 export default {
   components: {
     DashboardNavbar,
     ContentFooter
-    // FadeTransition
   },
   data() {
     return {
