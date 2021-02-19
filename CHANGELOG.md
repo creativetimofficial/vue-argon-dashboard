8ef761a4e202a1450fa95a304483c19ebdce281a
Toma Rares <tomairares@gmail.com>
2021-02-19 15:19:28 +0200

    Feat: add comments, readme, issue template and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## [1.1.1] - 2020-11-25
+- Change broken links
+
+## [1.1.0] - 2020-06-30
+- Package updates
+
+## [1.0.0] - 2019-04-07
+### Initial Release
--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+/*!
+
+=========================================================
+* Vue Argon Dashboard - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vue-argon-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/vue-argon-dashboard/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
