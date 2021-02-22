9ec691ffd741c2990274ec053992e0d12baa1fe9
Toma Rares <tomairares@gmail.com>
2021-02-22 16:02:59 +0200

    Feat: run lint

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ArgonDashboard from "./plugins/argon-dashboard";
-import 'element-plus/lib/theme-chalk/index.css';
+import "element-plus/lib/theme-chalk/index.css";
 
 const appInstance = createApp(App);
 appInstance.use(router);
